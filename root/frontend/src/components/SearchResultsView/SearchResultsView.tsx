8a0dca3fb12f030a649ee93339cb07b983d396b5
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-06-22 16:04:32 +0100

    Fix - convert divs to articles/ sections

--- root/frontend/src/components/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/components/SearchResultsView/SearchResultsView.tsx
@@ -1,13 +1,17 @@
 import "./SearchResultsView.css";
 export default function SearchResultsView() {
     return (
-        <>
-            <div className="srv-header">
+        <article>
+            <section className="srv-header">
                 <button className="srv-lists-button">Lists</button>
-                <input className="srv-searchbar" placeholder="Search catalogue" />
+                <input
+                    className="srv-searchbar"
+                    placeholder="Search catalogue"
+                />
                 <button className="srv-sort">Sort</button>
                 <button className="srv-filter">Filter</button>
-            </div>
-        </>
+            </section>
+            <section></section>
+        </article>
     );
 }
