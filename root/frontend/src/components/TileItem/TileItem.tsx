7b1679841382426372aec97cdb59edba6bead4f7
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-06-22 08:18:02 +0100

    Fix - add component structure
    Add logic for changing of the elements TileItem renders
    depending on whether TileItem is a ListItem or Search
    ResultItem. Add initial props for ListItem. Add basic
    element structure for SearchResultItem.

--- root/frontend/src/components/TileItem/TileItem.tsx
+++ root/frontend/src/components/TileItem/TileItem.tsx
@@ -1,4 +1,10 @@
-export const TileItem = () => {
+export const TileItem = ({
+  isList,
+  listDetails,
+}: {
+  isList: boolean;
+  listDetails: { name: string; itemCount: number };
+}) => {
   return (
     <article className="tile-container">
       <section className="tile-content-main">
@@ -7,6 +13,18 @@ export const TileItem = () => {
         <h4 className="tile-status"></h4>
       </section>
       <div className="tile-content-minor"></div>
+      {isList ? (
+        <section>
+          <h2>{listDetails.name}</h2>
+          <h4>{listDetails.itemCount}</h4>
+        </section>
+      ) : (
+        <span>
+          <div></div>
+          <div></div>
+          <div></div>
+        </span>
+      )}
     </article>
   );
 };
