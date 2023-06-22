1db1da7c4e9a43ddaf484238ca9e8828bc59ffdd
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-06-22 07:51:09 +0100

    Feature - TileItem initial commit
    Create TileItem component, add basic structure and elements

---
+++ root/frontend/src/components/TileItem/TileItem.tsx
@@ -0,0 +1,12 @@
+export const TileItem = () => {
+  return (
+    <article className="tile-container">
+      <section className="tile-content-main">
+        <img className="tile-preview" src="" alt="preview" />
+        <h2 className="tile-title"></h2>
+        <h4 className="tile-status"></h4>
+      </section>
+      <div className="tile-content-minor"></div>
+    </article>
+  );
+};
