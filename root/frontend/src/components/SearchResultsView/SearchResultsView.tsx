a5e1a88e17effbc0ce4d69f6332d700afd7d352d
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-07-01 11:32:01 +0100

    Chore - Add magnifying glass icon + minor CSS

--- root/frontend/src/components/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/components/SearchResultsView/SearchResultsView.tsx
@@ -1,6 +1,7 @@
 import "./SearchResultsView.css";
 import { TileItem } from "../TileItem/TileItem";
 import { Both } from "../../vite-env";
+
 export default function SearchResultsView() {
     const exampleSearchResults: Both[] = [
         {
@@ -34,22 +35,19 @@ export default function SearchResultsView() {
             genres: ["action", "adventure", "comedy", "fantasy"],
         },
     ];
-    // const exampleLists: Both[] = [
-    //     {
-    //         name: "Watching",
-    //         src: "some string",
-    //         numberOfItems: 5, // list.length
-    //     },
-    //     {
-    //         name: "Watched",
-    //         src: "some string",
-    //         numberOfItems: 9,
-    //     },
-    // ];
     return (
         <article>
             <section className="srv-header">
                 <button className="srv-lists-button">Lists</button>
+
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 512 512"
+                    className="icon"
+                >
+                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                </svg>
                 <input
                     className="srv-searchbar"
                     placeholder="Search catalogue"
@@ -62,9 +60,6 @@ export default function SearchResultsView() {
                 {exampleSearchResults.map((e) => {
                     return <TileItem details={e} />;
                 })}
-                {/*         {exampleLists.map((e) => {
-                    return <TileItem details={e} />;
-                })} */}
             </section>
             <button className="srv-top-button">Back to top</button>
         </article>
