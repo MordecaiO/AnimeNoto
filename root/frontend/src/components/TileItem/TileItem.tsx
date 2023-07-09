6fa112468fe6d5289a08f0ea9fa3846ab8d068d0
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-07-09 10:12:59 +0100

    Chore - Remove uneccessary comments

--- root/frontend/src/components/TileItem/TileItem.tsx
+++ root/frontend/src/components/TileItem/TileItem.tsx
@@ -2,12 +2,10 @@ import "./TileItem.css";
 
 import { Both } from "../../vite-env";
 export const TileItem = ({ details }: { details: Both }): JSX.Element => {
-    // const {name,src,...rest} = details
     const {name,/* src, */status, genres, numberOfItems,} = details
     return (
         <div className="menu-container">
             <article className="tile-container">
-                {/* <section className="tile-content-main"> */}
                 <div className="tile-preview">
                     <img src={`/test.image.jpg`} alt="preview" />
                     {genres && (
@@ -26,13 +24,6 @@ export const TileItem = ({ details }: { details: Both }): JSX.Element => {
                             : `${numberOfItems} items`}
                     </span>
                 </div>
-                {/* </section> */}
-                {/* <div className="tile-content-minor">
-                    <section>
-                        <h2>{details.name}</h2>
-                        <h4>{details.numberOfItems}</h4>
-                    </section>
-                </div> */}
             </article>
             <button className="context-button">...</button>
         </div>
