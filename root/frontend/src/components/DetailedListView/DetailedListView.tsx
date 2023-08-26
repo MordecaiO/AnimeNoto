e7e9f3cc5851d7e1cc5fdfa101ec554928659aca
mordecaioladimeji <cai.oladimeji@gmail.com>
2023-08-26 07:42:12 +0100

    fix - overflow x Wrap dlv in a styled div to solve overflow x property being applied to the whole page.

--- root/frontend/src/components/DetailedListView/DetailedListView.tsx
+++ root/frontend/src/components/DetailedListView/DetailedListView.tsx
@@ -3,75 +3,73 @@ import ListItem from "../ListItem/ListItem";
 import { ListType, ListItemType } from "../../vite-env";
 import { useLocation } from "react-router-dom";
 const exampleList: ListItemType[] = [
-    {
-        name: "Naruto",
-        status: "airing",
-        description: "The nine-tailed fox",
-        src: "",
-        genres: ["action", "shonen", "ninja"],
-        index: 0,
-    },
-    {
-        name: "Naruto",
-        status: "airing",
-        description: "The nine-tailed fox",
-        src: "",
-        genres: ["action", "shonen", "ninja"],
-        index: 1,
-    },
-    {
-        name: "Naruto",
-        status: "airing",
-        description: "The nine-tailed fox",
-        src: "",
-        genres: ["action", "shonen", "ninja"],
-        index: 2,
-    },
-    {
-        name: "Naruto",
-        status: "airing",
-        description: "The nine-tailed fox",
-        src: "",
-        genres: ["action", "shonen", "ninja"],
-        index: 3,
-    },
+  {
+    name: "Naruto",
+    status: "airing",
+    description: "The nine-tailed fox",
+    src: "",
+    genres: ["action", "shonen", "ninja"],
+    index: 0,
+  },
+  {
+    name: "Naruto",
+    status: "airing",
+    description: "The nine-tailed fox",
+    src: "",
+    genres: ["action", "shonen", "ninja"],
+    index: 1,
+  },
+  {
+    name: "Naruto",
+    status: "airing",
+    description: "The nine-tailed fox",
+    src: "",
+    genres: ["action", "shonen", "ninja"],
+    index: 2,
+  },
+  {
+    name: "Naruto",
+    status: "airing",
+    description: "The nine-tailed fox",
+    src: "",
+    genres: ["action", "shonen", "ninja"],
+    index: 3,
+  },
 ];
 function DetailedListView(): JSX.Element {
-    const {
-        // FIXME: listName: name,
-        listName,
-        dft,
-        lastUpdated,
-        listDescription,
-        listItems,
-    }: ListType = useLocation().state.listDetails;
-    const d = new Date(lastUpdated).toDateString()
-    return (
-        <div className="dlv-container">
-            <header className="dlv-header">
-                <div className="img-container">
-                    <img className="header_image"></img>
-                </div>
-                <section className="header-items">
-                    <p className="list-type">
-                        {dft ? "Default List" : "User List"}
-                    </p>
-                    <div className="details-container">
-                        <h1 className="list-name">{listName}</h1>
-                        <p className="list-desc">{listDescription}</p>
-                    </div>
-                    <p className="updated-text">
-                        {`Last updated: ${d}`}
-                    </p>
-                </section>
-            </header>
-            <main className="list-items-container">
-                {listItems.map((listitem, i) => {
-                    return <ListItem details={{ ...listitem, index: i }} />;
-                })}
-            </main>
-        </div>
-    );
+  const {
+    // FIXME: listName: name,
+    listName,
+    dft,
+    lastUpdated,
+    listDescription,
+    listItems,
+  }: ListType = useLocation().state.listDetails;
+  const d = new Date(lastUpdated).toDateString();
+  return (
+    <div className="container">
+      <div className="dlv-container">
+        <header className="dlv-header">
+          <div className="img-container">
+            <img className="header_image"></img>
+          </div>
+          <section className="header-items">
+            <p className="list-type">{dft ? "Default List" : "User List"}</p>
+            <div className="details-container">
+              <h1 className="list-name">{listName}</h1>
+              <p className="list-desc">{listDescription}</p>
+            </div>
+            <p className="updated-text">{`Last updated: ${d}`}</p>
+          </section>
+        </header>
+        <main className="list-items-container">
+          {listItems.map((listitem, i) => {
+            return <ListItem details={{ ...listitem, index: i }} />;
+          })}
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default DetailedListView;
