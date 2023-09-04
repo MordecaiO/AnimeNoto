4df44eecc70bad84fb90ae069a4bb541a95c67b9
Mordecai <cai.oladimeji@gmail.com>
2023-09-04 06:59:52 +0100

    Create first story file for TileItem with list = false ;wq :wq

---
+++ root/frontend/src/stories/TileItem.stories.tsx
@@ -0,0 +1,11 @@
+import { TileItem } from "../components/TileItem/TileItem"; 
+import {Meta, StoryObj} from "@storybook/react" 
+
+const meta : Meta<typeof TileItem> = {
+    component: TileItem
+} 
+export default meta ; 
+type Story = StoryObj<typeof TileItem> ; 
+export const FirstStory : Story = {
+    args : {list : false}
+}
