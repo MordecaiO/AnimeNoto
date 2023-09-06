0bc125435f60276807247f05566a54dcb4c1a557
Mordecai <cai.oladimeji@gmail.com>
2023-09-06 06:16:47 +0100

    feat - Create AnimeList component. Copy logic from TileItem into AnimeList

---
+++ root/frontend/src/components/AnimeList/AnimeList.tsx
@@ -0,0 +1,15 @@
+export const AnimeList = () => {
+  <div className="menu-container">
+    <article className="tile-container">
+      <>
+        <div className="tile-preview">
+          <img src={""} alt="preview" />
+        </div>
+        <div className="tile-details">
+          <span className="tile-title"></span>
+          <span className="tile-status">3</span>
+        </div>{" "}
+      </>
+    </article>
+  </div>;
+};
