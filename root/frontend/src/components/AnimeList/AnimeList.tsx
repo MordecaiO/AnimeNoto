bdde158394fbc132fe9129f9138fdab4133e0bba
MordecaiO <cai.oladimeji@gmail.com>
2023-10-07 10:06:23 +0100

    feat - add logic for conditional rendering of collage image

--- root/frontend/src/components/AnimeList/AnimeList.tsx
+++ root/frontend/src/components/AnimeList/AnimeList.tsx
@@ -1,5 +1,10 @@
-import { AnimeListProps } from "../../vite-env";
+import { AnimeListProps, AnimeProps } from "../../vite-env";
 
+const isCollage = (itemsArr: AnimeProps[] | undefined): string => {
+  if (!itemsArr) return "no items";
+
+  return itemsArr.length > 3 ? "collage" : "non-collage";
+};
 export const AnimeList = ({
   name,
   items,
@@ -10,6 +15,17 @@ export const AnimeList = ({
     <div className="menu-container">
       <article className="tile-container">
         <div className="tile-preview">
+          {isCollage(items) !== "no items" ? (
+            isCollage(items) === "collage" ? (
+              items?.slice(0, 4).map((item) => {
+                return <img src={item.images.jpg.image_url} />;
+              })
+            ) : (
+              <img src={items![0].images.jpg.image_url} />
+            )
+          ) : (
+            <img src="../../public/animenoto_logo.png" />
+          )}
           <img src={""} alt="preview" />
         </div>
         <div className="tile-details">
