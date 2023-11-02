7af86e32abe54f55676cc1df82dd25ac8c75dc5e
IndeedFlex <indeedflex@IndeedFlexs-MacBook-Pro.local>
2023-11-02 06:41:01 +0000

    fix - SRV layout & positioning

--- root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
@@ -40,7 +40,8 @@ export default function SearchResultsView({
     e.currentTarget.focus();
   };
   return (
-    <article>
+    <div className="wrapper">
+      <div className="container">
       <section className="srv-header">
         <div className="left inner-wrapper">
           <Link
@@ -53,14 +54,7 @@ export default function SearchResultsView({
           </Link>
         </div>
         <div className="centre inner-wrapper">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-            className="icon"
-          >
-            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-          </svg>
+          
           <form onSubmit={handleSubmit}>
             <input
               className="srv-searchbar"
@@ -70,12 +64,7 @@ export default function SearchResultsView({
             <button type="submit">Go</button>
           </form>
         </div>
-        <div className="right inner-wrapper">
-          <div className="srv-buttons">
-            <button className="srv-sort">Sort</button>
-            <button className="srv-filter">Filter</button>
-          </div>
-        </div>
+        
       </section>
       <section className="srv-results-container">
         {animes.map((anime: AnimeProps, i: number) => {
@@ -96,7 +85,8 @@ export default function SearchResultsView({
           );
         })}
       </section>
-      <button className="srv-top-button">Back to top</button>
-    </article>
+      {/* <button className="srv-top-button">Back to top</button> */}
+    </div>
+    </div>
   );
 }
