3d9e2fcec805f7c6c19f491ed8692a1bb51d224a
MordecaiO <cai.oladimeji@gmail.com>
2024-02-01 05:11:25 +0000

    feat - TileItemMenu behavior and styling Remove delete anime option and set list menu item as disabled if already present in the list. Reduce list menu item font size

--- root/frontend/src/components/TileItemMenu/TileItemMenu.tsx
+++ root/frontend/src/components/TileItemMenu/TileItemMenu.tsx
@@ -1,5 +1,6 @@
 import { Menu, MenuItem, SubMenu } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/core.css";
 import { AnimeListProps, TileItemMenuProps } from "../../vite-env";
 import "./TileItemMenu.css";
 
@@ -17,12 +18,16 @@ export const TileItemMenu = ({
       direction="top"
     >
       <MenuItem>More Info</MenuItem>
-      <SubMenu label="Add/Delete from List" direction="left">
+      <SubMenu label="Add to List" direction="left">
         {lists.map((list: AnimeListProps) => {
-          return !isAnimeInList(anime, list) ? (
+          return (
             <MenuItem
               className="add-menu-item"
-              onClick={() => handleAddAnime(list.id, anime, lists)}
+              disabled={isAnimeInList(anime, list)}
+              onClick={(e) => {
+                handleAddAnime(list.id, anime, lists);
+                e.keepOpen = false;
+              }}
             >
               Add to {list.name}
               <svg
@@ -35,29 +40,6 @@ export const TileItemMenu = ({
               >
                 <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
                 <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
-              </svg>
-            </MenuItem>
-          ) : (
-            <MenuItem
-              className="delete-menu-item"
-              onClick={() => handleDeleteAnime(list.id, anime, lists)}
-            >
-              Delete from {list.name}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="delete-icon"
-                height={15}
-                width={15}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
               </svg>
             </MenuItem>
           );
