81761c49660b6d33df8f2129aea01c3b98698a67
MordecaiO <cai.oladimeji@gmail.com>
2024-02-11 18:58:22 +0000

    Update CreateListModal.tsx

--- root/frontend/src/components/CreateListModal/CreateListModal.tsx
+++ root/frontend/src/components/CreateListModal/CreateListModal.tsx
@@ -17,7 +17,7 @@ export default function CreateListModal({
               <label className="name-label">Name</label>
               <input
                 className="name-input"
-                placeholder="Add a name"
+                placeholder="Add name"
                 required={true}
               ></input>
             </div>
