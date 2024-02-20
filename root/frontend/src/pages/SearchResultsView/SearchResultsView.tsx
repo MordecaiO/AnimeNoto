f98bba53ee2742dc80d5c11a3ed51141d78528b5
MordecaiO <cai.oladimeji@gmail.com>
2024-02-20 05:03:47 +0000

    header improvements

--- root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
@@ -48,16 +48,6 @@ export default function SearchResultsView({
               alt="AnimeNoto Logo"
             ></img>
           </div>
-          <div className="lists-button-wrapper">
-            <Link
-              to="lists"
-              state={{
-                lists: lists,
-              }}
-            >
-              <button className="lists-button">Lists</button>
-            </Link>
-          </div>
         </div>
         <div className="header-right">
           <div className="searchbar-wrapper">
@@ -68,12 +58,32 @@ export default function SearchResultsView({
                 name="search"
               />
               <button type="submit" className="searchbar-submit">
-                Search
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
               </button>
             </form>
           </div>
+          <div className="lists-button-wrapper">
+            <Link
+              to="lists"
+              state={{
+                lists: lists,
+              }}
+            >
+              <button className="lists-button">Lists</button>
+            </Link>
+          </div>
+          <a>Login</a>
+          <a>Sign up</a>
         </div>
-
       </header>
       <section className="results-container">
         {animes.map((anime: AnimeProps, i: number) => {
