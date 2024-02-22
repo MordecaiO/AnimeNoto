ab1e125299a5d2f02c86f10af4de98efb2c14947
MordecaiO <cai.oladimeji@gmail.com>
2024-02-22 05:47:37 +0000

    more header improvements

--- root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
@@ -40,50 +40,53 @@ export default function SearchResultsView({
   return (
     <div className="main">
       <header className="header">
-        <div className="header-left">
-          <div className="logo-wrapper">
-            <img
-              className="anime-noto"
-              src="../../animenoto_logo.png"
-              alt="AnimeNoto Logo"
-            ></img>
+        <nav className="navbar">
+          <div className="header-left">
+            <a className="logo-wrapper" href="/">
+              <img
+                className="anime-noto"
+                src="../../animenoto_logo.png"
+                alt="AnimeNoto Logo"
+              ></img>
+              <b className="logo-text">AnimeNoto</b>
+            </a>
           </div>
-        </div>
-        <div className="header-right">
-          <div className="searchbar-wrapper">
-            <form onSubmit={handleSubmit} className="searchbar-main">
-              <input
-                className="searchbar-term"
-                placeholder="Search catalogue"
-                name="search"
-              />
-              <button type="submit" className="searchbar-submit">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                  fill="currentColor"
-                  className="bibi-search"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                </svg>
-              </button>
-            </form>
+          <div className="header-right">
+            <div className="searchbar-wrapper">
+              <form onSubmit={handleSubmit} className="searchbar-main">
+                <input
+                  className="searchbar-term"
+                  placeholder="Search catalogue"
+                  name="search"
+                />
+                <button type="submit" className="searchbar-submit">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bibi-search"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                  </svg>
+                </button>
+              </form>
+            </div>
+            <div className="lists-button-wrapper">
+              <Link
+                to="lists"
+                state={{
+                  lists: lists,
+                }}
+              >
+                <button className="lists-button">Lists</button>
+              </Link>
+            </div>
+            <a className="login">Login</a>
+            <a className="sign-up">Sign up</a>
           </div>
-          <div className="lists-button-wrapper">
-            <Link
-              to="lists"
-              state={{
-                lists: lists,
-              }}
-            >
-              <button className="lists-button">Lists</button>
-            </Link>
-          </div>
-          <a>Login</a>
-          <a>Sign up</a>
-        </div>
+        </nav>
       </header>
       <section className="results-container">
         {animes.map((anime: AnimeProps, i: number) => {
