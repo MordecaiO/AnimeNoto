11310b93111d90dc6beb1d31f3a756511403f41e
MordecaiO <cai.oladimeji@gmail.com>
2024-03-15 04:40:00 +0000

    fix - rel file path

--- root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
+++ root/frontend/src/pages/SearchResultsView/SearchResultsView.tsx
@@ -47,7 +47,7 @@ export default function SearchResultsView({
             <a className="logo-wrapper" href="/">
               <img
                 className="anime-noto"
-                src="/animenoto_logo_main.jpeg"
+                src="/AnimeNoto/animenoto_logo_main.jpeg"
                 alt="AnimeNoto Logo"
               ></img>
               <b className="logo-text">AnimeNoto</b>
