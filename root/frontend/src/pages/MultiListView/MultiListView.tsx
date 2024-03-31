e33f2ce6ed00121f1372a7810d480c7416c00dbd
MordecaiO <cai.oladimeji@gmail.com>
2024-03-31 10:51:46 +0100

    chore(MultiListView): add tooltip to MLV header

--- root/frontend/src/pages/MultiListView/MultiListView.tsx
+++ root/frontend/src/pages/MultiListView/MultiListView.tsx
@@ -19,7 +19,7 @@ export default function MultiListView({
   const [isOpen, setOpen] = useState(false);
   const [isEditing, setEditing] = useState(false);
   const [selectedList, setSelectedList] = useState(animeLists[0]);
-  const { logout } = useAuth0();
+  const { logout, isAuthenticated, user } = useAuth0();
   return (
     <>
       {isOpen && (
@@ -60,21 +60,26 @@ export default function MultiListView({
               <a onClick={() => logout()} className="logout-link">
                 Logout
               </a>
-              <svg
-                className="user-icon"
-                fill="none"
-                height="24"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
+              <div className="user-icon-container">
+                <svg
+                  className="user-icon"
+                  fill="none"
+                  height="24"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                  <circle cx="12" cy="7" r="4" />
+                </svg>
+                {isAuthenticated && (
+                  <span className="user tooltip"> {user?.email} </span>
+                )}
+              </div>
             </div>
           </nav>
         </header>
