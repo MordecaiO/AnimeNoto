18e394090510f9caf4674fe42ede80367e9ca22f
MordecaiO <cai.oladimeji@gmail.com>
2024-04-03 19:20:36 +0100

    chore: add test route

--- root/backend/db/connection.js
+++ root/backend/db/connection.js
@@ -1,5 +1,4 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import "dotenv/config";
 const uri = process.env.ATLAS_URI || "";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -10,18 +9,17 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (err) {
+  console.error(err);
 }
-run().catch(console.dir);
+
+let db = client.db();
+
+export default db;
